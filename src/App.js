e7d8887085758139659ed111e42dfd73948f6699
Eladdahan20 <eladdahan20@gmail.com>
2024-07-22 11:16:13 +0300

    Outputting Answers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header.jsx';
+import Quiz from './Components/Quiz.jsx';
 
 function App() {
   return (
+    <>
     <Header />
+    <menu>
+      <Quiz />
+    </menu>
+    </>
   );
 }
 
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,9 +1,29 @@
-import { useState } from 'react';
-import QUESTIONS from '../questions';
+import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-    const [userAnswers, setUserAnswers] = useState([])
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
 
-    return <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
